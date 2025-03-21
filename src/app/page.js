225fbf8796bef9815c5cce7570ff7cf267bdda3e
mukulsingh94868 <mukulsingh94868@gmail.com>
2025-03-21 18:10:27 +0530

    changes

--- src/app/page.js
+++ src/app/page.js
@@ -1,8 +1,24 @@
+import Image from "next/image";
+import Button from "../components/Button/Button";
+import styles from "./page.module.css";
+// import Hero from "public/hero.png";
 
 export default function Home() {
   return (
-    <div>
-      Hello World
+    <div className={styles.container}>
+      <div className={styles.item}>
+        <h1 className={styles.title}>
+          Better design for your digital products.
+        </h1>
+        <p className={styles.desc}>
+          Turning your Idea into Reality. We bring together the teams from the
+          global tech industry.
+        </p>
+        <Button url="/portfolio" text="See Our Works" />
+      </div>
+      <div className={styles.item}>
+        <Image src={'/hero.png'} alt="" className={styles.img} height={500} width={500} />
+      </div>
     </div>
   );
 }
